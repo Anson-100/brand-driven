7808216a160df1e5bc62d4ab9066475c0734f409
Anson Hunnell <ansonhunnell@gmail.com>
2025-01-22 15:09:03 -0500

    just in case

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform, View, Text } from "react-native"
+import { StyleSheet, SafeAreaView, View, Text } from "react-native"
 
 import { HelloWave } from "@/components/HelloWave"
 import ParallaxScrollView from "@/components/ParallaxScrollView"
@@ -7,9 +7,9 @@ import { ThemedView } from "@/components/ThemedView"
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center bg-blue-500">
+    <SafeAreaView className="flex-1 justify-center items-center bg-blue-400">
       <Text className="text-white text-lg font-bold">Tailwind Works! 🎉</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
