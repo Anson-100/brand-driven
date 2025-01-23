cee9bdd59235096a3f07fd2f429352c44cacd9c7
Anson Hunnell <ansonhunnell@gmail.com>
2025-01-23 16:45:09 -0500

    update files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,5 @@
 import { Image, StyleSheet, Platform, View, Text } from "react-native"
 
-import { HelloWave } from "@/components/HelloWave"
-import ParallaxScrollView from "@/components/ParallaxScrollView"
-import { ThemedText } from "@/components/ThemedText"
-import { ThemedView } from "@/components/ThemedView"
-
 export default function HomeScreen() {
   return (
     <View className="flex-1 justify-center items-center bg-blue-500">
