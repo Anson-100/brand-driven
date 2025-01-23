8ce0b5c2623935d66d4e9d9d64a235b006106820
Anson Hunnell <ansonhunnell@gmail.com>
2025-01-23 16:17:15 -0500

    tailwind working still; working on file structure

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text className="h-full bg-gray-600 text-gray-50">
+        Homedddddddddddddddddddd
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
